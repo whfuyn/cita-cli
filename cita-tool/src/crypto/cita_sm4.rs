e9f65b1193d7554e05a0f237b5c5286d1474bee5
whfuyn <whfuyn@outlook.com>
2021-06-22 09:46:35 +0000

    use cbc mode

--- cita-tool/src/crypto/cita_sm4.rs
+++ cita-tool/src/crypto/cita_sm4.rs
@@ -5,7 +5,7 @@ pub fn sm4_encrypt(plaintext: &[u8], password: &str) -> Vec<u8> {
     let pwd_hash = sm3::hash::Sm3Hash::new(password.as_bytes()).get_hash();
 
     let (key, iv) = pwd_hash.split_at(16);
-    let cipher = sm4::Cipher::new(key, sm4::Mode::Cfb);
+    let cipher = sm4::Cipher::new(key, sm4::Mode::Cbc);
 
     cipher.encrypt(plaintext, iv)
 }
@@ -15,7 +15,7 @@ pub fn sm4_decrypt(ciphertext: &[u8], password: &str) -> Vec<u8> {
     let pwd_hash = sm3::hash::Sm3Hash::new(password.as_bytes()).get_hash();
 
     let (key, iv) = pwd_hash.split_at(16);
-    let cipher = sm4::Cipher::new(key, sm4::Mode::Cfb);
+    let cipher = sm4::Cipher::new(key, sm4::Mode::Cbc);
 
     cipher.decrypt(ciphertext, iv)
 }
